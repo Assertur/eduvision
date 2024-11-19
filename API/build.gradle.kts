139b46b1677749cc330bb60b83a3e5540d8fc4cf
Assertur <asserche@gmail.com>
2024-11-18 21:18:19 -0500

    init backend

---
+++ API/build.gradle.kts
@@ -0,0 +1,48 @@
+plugins {
+	kotlin("jvm") version "1.9.25"
+	kotlin("plugin.spring") version "1.9.25"
+	id("org.springframework.boot") version "3.3.4"
+	id("io.spring.dependency-management") version "1.1.6"
+	kotlin("plugin.jpa") version "1.9.25"
+}
+
+group = "com.eduvision"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(17)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	// Spring Boot Starters
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	// Jackson for Kotlin
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	// Kotlin
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	// MariaDB JDBC Driver
+	implementation("org.mariadb.jdbc:mariadb-java-client:3.4.1")
+	// JBoss Logging
+	implementation("org.jboss.logging:jboss-logging:3.5.0.Final")
+
+	// Testing
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+}
+
+kotlin {
+	compilerOptions {
+		freeCompilerArgs.addAll("-Xjsr305=strict")
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ API/src/test/kotlin/com/eduvision/API/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.eduvision.API
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
